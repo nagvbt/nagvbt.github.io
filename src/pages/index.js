640779b80e7ce1b25d7c22aad06ab8f738d42703
Nag VBT <nagavenkatabalaji.thota@sumtotalsystems.com>
2023-04-28 01:38:17 +0530

    Typed

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,44 +6,56 @@ import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 import styles from "./index.module.css";
 import Socials from "../components/Socials/Socials";
+import Typed from "../components/Typed";
 
 function HomepageHeader() {
   return (
     <header className={styles.hero}>
       <div className="container">
-        <div className="row">
-          <div className="col col--6">
-            <h1>Nag VBT</h1>
-            <p>M.Tech, M.B.A, B.Tech, L.L.B</p>
-            <p className="hero__subtitle">Architect . Innovator . Mentor</p>
-            <div className={styles.buttons}>
-              <div className={styles.link}>
-                <Link className="button button--primary button--lg" to="/about">
-                  About Me
+        <section>
+          <div className="row">
+            <div className="col col--6">
+              <h1>Nag VBT</h1>
+              <p>M.Tech, M.B.A, B.Tech, L.L.B</p>
+              <p className="hero__subtitle">
+                <Typed
+                  strings={["Architect . Innovator . Mentor"]}
+                  typeSpeed={75}
+                />
+              </p>
+              {/* <p className="hero__subtitle">Architect . Innovator . Mentor</p> */}
+              <div className={styles.buttons}>
+                <div className={styles.link}>
+                  <Link
+                    className="button button--primary button--lg"
+                    to="/about"
+                  >
+                    About Me
+                  </Link>
+                </div>
+                <Link
+                  className="button button--secondary button--lg"
+                  to="/docs/intro"
+                >
+                  Articles
                 </Link>
               </div>
-              <Link
-                className="button button--secondary button--lg"
-                to="/docs/intro"
-              >
-                Articles
-              </Link>
+              <p className="padding-top--md">
+                Explore <strong>Web</strong>, <strong>Mobile</strong>,{" "}
+                <strong>Microservices</strong>, <strong>Destop</strong>, and{" "}
+                <strong>AI</strong> architectures
+              </p>
             </div>
-            <p className="padding-top--md">
-              Explore <strong>Web</strong>, <strong>Mobile</strong>,{" "}
-              <strong>Microservices</strong>, <strong>Destop</strong>, and{" "}
-              <strong>AI</strong> architectures
-            </p>
+            <div className="col col--6">
+              <img
+                className={styles.logo}
+                src={useBaseUrl("/img/logo.png")}
+                alt="Gotenberg Hero Logo"
+              />
+              <Socials />
+            </div>
           </div>
-          <div className="col col--6">
-            <img
-              className={styles.logo}
-              src={useBaseUrl("/img/logo.png")}
-              alt="Gotenberg Hero Logo"
-            />
-            <Socials />
-          </div>
-        </div>
+        </section>
       </div>
     </header>
   );
@@ -56,8 +68,8 @@ export default function Home() {
       title={`${siteConfig.title}`}
       description="Description will go into a meta tag in <head />"
     >
-      <HomepageHeader />
       <main>
+        <HomepageHeader />
         <HomepageFeatures />
       </main>
     </Layout>
