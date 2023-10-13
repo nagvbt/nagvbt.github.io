0abacfa50334999999deff28ff33d9021b337901
nthota <STRama@743>
2023-10-14 00:01:41 +0530

    Deploy website - based on 817872209d1f6a51516405352bc2040224fdfc5d

---
+++ assets/js/98b657d8.adc5fb99.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunknagvbt=self.webpackChunknagvbt||[]).push([[38486],{15509:e=>{e.exports=JSON.parse('{"permalink":"/blog/page/8","page":8,"postsPerPage":10,"totalPages":16,"totalCount":153,"previousPage":"/blog/page/7","nextPage":"/blog/page/9","blogDescription":"Blog","blogTitle":"Blog"}')}}]);
--- assets/js/98b657d8.ca32c25b.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunknagvbt=self.webpackChunknagvbt||[]).push([[38486],{15509:e=>{e.exports=JSON.parse('{"permalink":"/blog/page/8","page":8,"postsPerPage":10,"totalPages":16,"totalCount":152,"previousPage":"/blog/page/7","nextPage":"/blog/page/9","blogDescription":"Blog","blogTitle":"Blog"}')}}]);
