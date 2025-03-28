64321b6339124ccaff52df431f616b3c263265da
nagvbt <nagvbt@example.com>
2025-03-28 20:35:04 +0530

    Refactor ESLint configuration: remove old files and implement new config structure

---
+++ eslint.config.js
@@ -0,0 +1,57 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([globalIgnores([
+    "**/package-lock.json",
+    "**/.docusaurus",
+    "**/.github",
+    "**/node_modules",
+    "**/blog",
+    "**/docs",
+    "**/static",
+    "**/build",
+    "**/coverage",
+]), {
+    extends: compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"),
+
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        parser: tsParser,
+        ecmaVersion: 2018,
+        sourceType: "module",
+
+        parserOptions: {
+            ecmaFeatures: {
+                jsx: true,
+            },
+        },
+    },
+
+    rules: {
+        "@typescript-eslint/no-var-requires": 0,
+        "@typescript-eslint/ban-types": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-non-null-assertion": "off",
+        "@typescript-eslint/no-unused-vars": "off",
+        "no-prototype-builtins": "off",
+        "no-useless-escape": "off",
+        "prefer-spread": "off",
+    },
+}]);
